ca106582f69705891ee78c8cd121e2761ed3147f
sarah pioline benourdja <sarahpiolinebenourdja@gmail.com>
2020-12-12 16:15:09 +0100

    création d'une nouvelle branche

--- migrations/Version20201212150223.php
+++ migrations/Version20201212150223.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201205120034 extends AbstractMigration
+final class Version20201212150223 extends AbstractMigration
 {
     public function getDescription() : string
     {
