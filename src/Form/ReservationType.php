4ffca2717ad10fc8d109c88ad5416526649752b6
Thomas Heuveline <thomasheuveline61@gmail.com>
2020-12-12 16:39:48 +0100

    installation security

---
+++ src/Form/ReservationType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reservation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('arrival_date_reservation')
+            ->add('departude_date_reservation')
+            ->add('date_reservation')
+            ->add('reservation_housing')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Reservation::class,
+        ]);
+    }
+}
