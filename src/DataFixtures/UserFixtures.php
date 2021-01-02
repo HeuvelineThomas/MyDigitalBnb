dd7a0795b200fb1333b05e01e16129e92d38a084
Sarah Pioline <sarahpiolinebenourdja@gmail.com>
2021-01-02 16:46:01 +0100

    crud user

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+         $user = new User();
+         $manager->persist($product);
+
+        $manager->flush();
+    }
+}
